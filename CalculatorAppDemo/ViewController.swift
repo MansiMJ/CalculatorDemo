269b736de7da00f9841ef0ec3a559dd9ca6ff1ca
MansiMJ <72232020+MansiMJ@users.noreply.github.com>
2020-10-02 01:40:27 -0400

    Add files via upload

---
+++ CalculatorAppDemo/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CalculatorAppDemo
+//
+//  Created by Mansi Majethia on 2020-10-01.
+//  Copyright © 2020 CentennialCollege. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
